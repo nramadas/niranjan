6c15fe409685d15675ddf7efd1c770f2b8d1b88a
Niranjan Ramadas <nrbramadas@gmail.com>
2020-02-06 15:41:41 -0800

    Add CORS

--- api/index.js
+++ api/index.js
@@ -1,7 +1,22 @@
+const cors = require('cors');
 const express = require('express');
 
 const gql = require('./gql');
 
 const app = express();
+const VALID_ORIGINS = new Set(['www.niranjan.me', 'niranjan.me']);
+
+// enable CORS
+app.use(
+  cors({
+    origin: (origin, callback) =>
+      VALID_ORIGINS.has(origin) || process.env.NODE_ENV !== 'production'
+        ? callback(null, true)
+        : callback(new Error('Not Allowed (CORS)')),
+  }),
+);
+
+// attach gql
 app.use('/', gql);
+
 app.listen(process.env.GQL_PORT);
